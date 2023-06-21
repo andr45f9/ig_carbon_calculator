e79f32f054ee3153fa57de9e73f3af23eb5b8f08
Andrea <andr45f9@stud.kea.dk>
2023-06-21 14:34:32 +0200

    build

--- src/components/InfoArticle.jsx
+++ src/components/InfoArticle.jsx
@@ -4,17 +4,17 @@ function InfoArticle() {
   return (
     <div className="InfoContainer">
       <article>
-        <img src="./src/img/info_cloud1.webp" alt="A gradient cloud illustration" />
+        <img src="./img/info_cloud1.webp" alt="A gradient cloud illustration" />
         <p>Have you ever wondered what the environmental cost of a Instagram post, a story, watching a live or scrolling through Instagram feed could be?</p>
       </article>
 
       <article>
-        <img src="./src/img/info_cloud2.webp" alt="A gradient cloud illustration" />
+        <img src="./img/info_cloud2.webp" alt="A gradient cloud illustration" />
         <p>The application launched in 2010 and has 1 billion monthly active users including 28 million unique visitors per month. Instagram is the most frequented social network behind Facebook.</p>
       </article>
 
       <article>
-        <img src="./src/img/info_cloud3.webp" alt="A gradient cloud illustration" />
+        <img src="./img/info_cloud3.webp" alt="A gradient cloud illustration" />
         <p>On average and per day, the carbon impact of a user on Instagram is 18.6 gEqCO2, or the equivalent of 166 meters traveled by a light vehicle.</p>
       </article>
     </div>
